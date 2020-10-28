2b5cabe1879f1ea456b7f511d08e81851fb1c7b3
manemin <manemine80@gmail.com>
2020-10-28 09:53:36 +0200

    done

--- lesson1/src/components/user.js
+++ lesson1/src/components/user.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import './user.css'
+
 class User extends Component {
     // constructor(props) {
     //     super(props);
@@ -8,9 +8,10 @@ class User extends Component {
         const { usr } = this.props;
         return (
             <li>
-                id: <span>{usr.id}</span>, name: <span>{usr.name}</span>, status: <span>{usr.status.toString()}</span>
+                id: <span>{usr.id}</span>,
+                name: <span>{usr.name}</span>,
+                status: <span>{usr.status.toString()}</span>
             </li>
-
         );
     }
 }
